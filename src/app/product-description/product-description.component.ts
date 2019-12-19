fc8ac88b06111fe9a721e649c828d33a70e48970
stetsonh-tyler <59056965+stetsonh-tyler@users.noreply.github.com>
2019-12-19 12:57:20 -0500

    part3 - moved albumInfo declaration above constructor

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,13 +8,14 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor(private _productService: ProductService) {
-  }
-  
   albumInfo;
 
+  constructor(private _productService: ProductService) {
+  }
+
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe((response) => this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response => 
+      this.albumInfo = response);
   }
 
 }
